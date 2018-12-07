cdb2766b198f36ce2aa2e49c21118b4390404fb1
Jake Howard <git@theorangeone.net>
2018-12-07 21:10:52 +0000

    Run linters on setup.py

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,9 +4,9 @@ set -e
 
 export PATH=env/bin:${PATH}
 
-python setup.py test
+nose2 tests
 
-black tests ipc_unix --check
-flake8 ipc_unix tests --ignore=E128,E501
-isort -rc -c ipc_unix tests
+black tests ipc_unix setup.py --check
+flake8 ipc_unix tests setup.py --ignore=E128,E501
+isort -rc -c ipc_unix tests setup.py
 mypy --strict-optional ipc_unix tests
--- setup.py
+++ setup.py
@@ -1,22 +1,16 @@
 from setuptools import setup
-import unittest
 
 setup(
     name="ipc-unix",
     version="0.0.0",
     url="https://github.com/realorangeone/ipc-unix",
-    license='MIT',
+    license="MIT",
     author="Jake Howard",
     description="Inter-Process Communication using unix sockets",
-    packages=['ipc_unix'],
+    packages=["ipc_unix"],
     include_package_data=True,
     zip_safe=False,
-    pathon_requires='>=3.4',
-    install_requires=[
-        'ujson'
-    ],
-    project_urls={
-        'GitHub: Issues': 'https://github.com/realorangeone/ipc-unix/issues'
-    },
-    test_suite="nose2.collector.collector"
+    pathon_requires=">=3.4",
+    install_requires=["ujson"],
+    project_urls={"GitHub: Issues": "https://github.com/realorangeone/ipc-unix/issues"},
 )
